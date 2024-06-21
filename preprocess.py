0c1a16fd288d924d9ea3e38637fe174a9e612b73
ismozi <ismozi@github.com>
2024-06-21 16:29:42 -0400

    Preprocess goal distribution

--- preprocess.py
+++ preprocess.py
@@ -27,3 +27,44 @@ def load_and_preprocess_data(file_path):
     pivot_df = pivot_df.rename_axis(None, axis=1)
 
     return pivot_df
+
+
+def get_team_goal_distribution(team_id):
+    match_events_df = pd.read_excel('./static/EURO_2020_DATA.xlsx', sheet_name='Match events')
+
+    team_goals = match_events_df[((match_events_df['TeamFromID'] == team_id) & 
+                                 (match_events_df['Event'].isin(['Goal', 'GoalOnPenalty']))) | 
+                                 ((match_events_df['TeamToID'] == team_id) & 
+                                 (match_events_df['Event'] == 'OwnGoal'))]
+    
+    first_half_goals = team_goals[team_goals['Phase'] == 1].shape[0]
+    second_half_goals = team_goals[team_goals['Phase'] == 2].shape[0]
+    ot_goals = team_goals[team_goals['Phase'] >= 3].shape[0]
+    
+    return pd.Series({
+        'FirstHalfGoals': first_half_goals,
+        'SecondHalfGoals': second_half_goals,
+        'OTGoals': ot_goals
+    })
+
+def get_goal_distribution_df():
+    match_stats_df = pd.read_excel('./static/EURO_2020_DATA.xlsx', sheet_name='Match Stats')
+    id_name_df = match_stats_df.drop_duplicates(subset=['TeamID', 'TeamName'])[['TeamID', 'TeamName']]
+    
+    team_ids = match_stats_df['TeamID'].unique()
+
+    goal_distribution_df = pd.DataFrame(index=team_ids, columns=['FirstHalfGoals', 'SecondHalfGoals', 'OTGoals'])
+
+    for team_id in team_ids:
+        goal_distribution_df.loc[team_id] = get_team_goal_distribution(team_id)
+
+    goal_distribution_df = goal_distribution_df.merge(id_name_df, left_index=True, right_on='TeamID')
+
+    goal_distribution_df.set_index('TeamName', inplace=True)
+    goal_distribution_df = goal_distribution_df[['FirstHalfGoals', 'SecondHalfGoals', 'OTGoals']]
+    
+    print(goal_distribution_df)
+
+
+
+get_goal_distribution_df()
