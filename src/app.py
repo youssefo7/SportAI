ce452cb1a7813e2dc75f953357aca92c27745ebd
KinanToutoungy <ktoutoungy@gmail.com>
2024-06-22 15:52:15 -0400

    Keep tabs for 3D scatter plot only

--- src/app.py
+++ src/app.py
@@ -5,7 +5,8 @@ from dash import dcc
 from dash import html
 import plotly.express as px
 from preprocess import load_and_preprocess_data, get_goal_distribution_df
-from graphs import create_offensive_3d_scatter_plot, create_defensive_3d_scatter_plot, create_parallel_coordinates_plot, create_goal_dist_bar_chart, create_radar_chart
+from graphs import create_offensive_3d_scatter_plot, create_defensive_3d_scatter_plot, create_parallel_coordinates_plot, \
+    create_goal_dist_bar_chart, create_radar_chart
 import os
 
 server = Flask(__name__)
@@ -33,32 +34,63 @@ app.layout = html.Div([
             style={'width': '95%'}
         ),
     ], style={'display': 'flex', 'padding': '10px'}),
-    
+
     dcc.Tabs(id='tabs-example', value='tab-1', children=[
         dcc.Tab(label='Offensive Performance', value='tab-1', style={'padding': '10px'}),
         dcc.Tab(label='Defensive Performance', value='tab-2', style={'padding': '10px'}),
-        dcc.Tab(id='parallel-coordinates',label='Parallel Coordinates', value='tab-3', style={'padding': '10px'}),
-        dcc.Tab(label='Radar Chart', value='tab-4', style={'padding': '10px'}),
-        dcc.Tab(label='Goal distribution', value='tab-5', style={'padding': '10px'}),
     ]),
     dcc.Graph(id='graph-content', style={'height': '80vh', 'width': '100%'}),
+    dcc.Graph(id='parallel_coordinates_plot', style={'height': '80vh', 'width': '100%'}),
+    html.Div(id='compare-title', style={'padding': '10px'}),
+    html.Div([
+        dcc.Dropdown(
+            id='team-dropdown-compare',
+            options=[{'label': team, 'value': team} for team in team_stats['TeamName'].unique()],
+            value='',
+            style={'width': '95%'}
+        ),
+        dcc.Graph(id='radar-chart', style={'height': '80vh', 'width': '100%'})
+    ], style={'padding': '10px'}),
+    dcc.Graph(id='goal_dist_bar_chart', style={'height': '80vh', 'width': '100%'}),
+
     html.Div(id='hover-data')
 ])
+
 
 @app.callback(Output('graph-content', 'figure'),
               [Input('tabs-example', 'value'),
                Input('team-dropdown', 'value')])
-def render_content(tab, selected_team):
+def render_3d_scatter_plot(tab, selected_team):
     if tab == 'tab-1':
         return create_offensive_3d_scatter_plot(team_stats)
     elif tab == 'tab-2':
         return create_defensive_3d_scatter_plot(team_stats)
-    elif tab == 'tab-3':
-        return create_parallel_coordinates_plot(team_stats,selected_team)
-    elif tab == 'tab-4':
-        return create_radar_chart(team_stats, selected_team)
-    elif tab == 'tab-5':
-        return create_goal_dist_bar_chart(goal_dist, selected_team)
+
+
+@app.callback(Output('parallel_coordinates_plot', 'figure'),
+              [Input('team-dropdown', 'value')])
+def render_parallel_coordinates_plot(selected_team):
+    return create_parallel_coordinates_plot(team_stats, selected_team)
+
+
+@app.callback(Output('compare-title', 'children'),
+              [Input('team-dropdown', 'value')])
+def update_compare_title(selected_team):
+    return html.Label(f"Select a team to compare to {selected_team}:", style={'padding': '10px'})
+
+
+@app.callback(Output('radar-chart', 'figure'),
+              [Input('team-dropdown', 'value'),
+               Input('team-dropdown-compare', 'value')])
+def render_radar_chart(selected_team, selected_team_to_compare):
+    return create_radar_chart(team_stats, selected_team, selected_team_to_compare)
+
+
+@app.callback(Output('goal_dist_bar_chart', 'figure'),
+              [Input('team-dropdown', 'value')])
+def render_goal_dist_bar_chart(selected_team):
+    return create_goal_dist_bar_chart(goal_dist, selected_team)
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
