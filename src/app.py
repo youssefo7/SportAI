9596e70d6ccd95630ac415084ff1152ebbabf370
chenak-a <chenak.alaa1@gmail.com>
2024-06-21 23:01:00 -0400

    select contry

--- src/app.py
+++ src/app.py
@@ -16,13 +16,18 @@ file_path = os.path.join(os.path.dirname(__file__), 'static', 'EURO_2020_DATA.xl
 
 team_stats = load_and_preprocess_data(file_path)
 
-team_stats = team_stats.astype({'Ball Possession': 'int32', 'Total Attempts': 'int32', 'Goals': 'int32'})
-offensive_fig = create_offensive_3d_scatter_plot(team_stats)
-defensive_fig = create_defensive_3d_scatter_plot(team_stats)
-parallel_fig = create_parallel_coordinates_plot(team_stats)
-
 app.layout = html.Div([
     html.H1("UEFA Euro 2020 Team Performance in 3D", style={'textAlign': 'center', 'padding': '20px'}),
+    html.Div([
+        html.Label("Select a team:", style={'padding': '10px'}),
+        dcc.Dropdown(
+            id='team-dropdown',
+            options=[{'label': team, 'value': team} for team in team_stats['TeamName'].unique()],
+            value='',
+            style={'width': '95%'}
+        ),
+    ], style={'display': 'flex', 'padding': '10px'}),
+    
     dcc.Tabs(id='tabs-example', value='tab-1', children=[
         dcc.Tab(label='Offensive Performance', value='tab-1', style={'padding': '10px'}),
         dcc.Tab(label='Defensive Performance', value='tab-2', style={'padding': '10px'}),
@@ -32,13 +37,18 @@ app.layout = html.Div([
 ])
 
 @app.callback(Output('tabs-content-example', 'children'),
-              Input('tabs-example', 'value'))
-def render_content(tab):
+              [Input('tabs-example', 'value'),
+               Input('team-dropdown', 'value')])
+def render_content(tab, selected_team):
+    print(selected_team)
     if tab == 'tab-1':
+        offensive_fig = create_offensive_3d_scatter_plot(team_stats)
         return dcc.Graph(figure=offensive_fig)
     elif tab == 'tab-2':
+        defensive_fig = create_defensive_3d_scatter_plot(team_stats)
         return dcc.Graph(figure=defensive_fig)
     elif tab == 'tab-3':
+        parallel_fig = create_parallel_coordinates_plot(team_stats)
         return dcc.Graph(figure=parallel_fig)
 
 if __name__ == '__main__':
