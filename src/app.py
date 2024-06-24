ff1b945ee3cf68ee83c3cdf49cc21f0b0c09bc74
EL-FIGHA <79546158+oussamaelfig@users.noreply.github.com>
2024-06-23 21:00:50 -0400

    change port to 0.0.0.0

--- src/app.py
+++ src/app.py
@@ -13,14 +13,15 @@ file_path = os.path.join(os.path.dirname(__file__), 'static', 'EURO_2020_DATA.xl
 preprocessor = Preprocessor(file_path)
 
 app = Dash(__name__, use_pages=True, server=server, url_base_pathname='/', external_stylesheets=[
-        dbc.themes.BOOTSTRAP,
-        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css'  # Include Font Awesome
-    ], )
+    dbc.themes.BOOTSTRAP,
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css'  # Include Font Awesome
+], )
 
 app.layout = html.Div([
     html.H1("UEFA Euro 2020 Team Performance", style={'textAlign': 'center', 'padding': '20px'}),
-    page_container # Placeholder for page content
+    page_container  # Placeholder for page content
 ])
-    
-if __name__ == '__main__':
-    app.run_server()
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
